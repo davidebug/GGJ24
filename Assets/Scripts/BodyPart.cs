13b9be89b06d0743bc34a86c3516ec03b958ffa2
salvo gurzi <sgurzi@34bigthings.com>
2024-01-27 18:46:34 +0100

    BodyPart - added object selection

--- Assets/Scripts/BodyPart.cs
+++ Assets/Scripts/BodyPart.cs
@@ -11,54 +11,104 @@ public class BodyPart : MonoBehaviour, IPointerClickHandler, IPointerEnterHandle
     public Image bodyImage;
     // image identification number
     public int bodyNumberIndex;
-
+    private Color original;
     private bool IsSelected;
+    private bool IsShowingHoveringEffect;
+    private GameManager gameManager;    
     private void Awake()
     {
         IsSelected = false;  
         bodyImage = GetComponent<Image>();
         Assert.IsNotNull(bodyImage);    
-    }
-    void Start()
-    {
-        
+        original = bodyImage.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        gameManager = GameManager.Get();
     }
+    private void OnEnable()
+    {
+        if(gameManager == null)
+        {
+            gameManager = GameManager.Get();
+        }
+
+        gameManager.OnWrongSequence += Unselect;
+
+    }
+
 
     public void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        //SetBodyPartState()   
+        SelectBodyPart(); 
     }
 
     public void OnPointerEnter(UnityEngine.EventSystems.PointerEventData eventData)
     {
         Debug.Log($"Pointer entered on Image {bodyNumberIndex}");
-
+        ShowHoverEffect(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log($"Pointer exited on Image {bodyNumberIndex}");
+        ShowHoverEffect(false);
     }
 
-    public void SetBodyPartState(bool select)
+    private void SelectBodyPart()
     {
-        if (select && !IsSelected)
+        if (!IsSelected)
         {
-            ShowClickedEffect();
+            if (gameManager.TryToSelectBodyPart(bodyNumberIndex))
+            {
+                ShowClickedEffect(true);
+                IsSelected=true;
+            }
+
+        }
+        else
+        {
+            gameManager.UnselectEverything();
+            IsSelected = false;
+            ShowClickedEffect(false);
         }
         
     }
 
-    private void ShowClickedEffect()
+    private void ShowClickedEffect(bool select)
     {
-        bodyImage.color = Color.green;
+        if (select)
+        {
+            bodyImage.color = Color.green;
+
+        }
+        else
+        {
+            bodyImage.color = Color.red;    
+        }
     }
 
+    private void Unselect()
+    {
+        IsSelected = false;
+        bodyImage.color = original;
+    }
     
+    private void ShowHoverEffect(bool isPointerHovering)
+    {
+        if (isPointerHovering == IsShowingHoveringEffect || IsSelected)
+            return;
+
+        if (isPointerHovering)
+        {
+            Debug.Log("Set Hovering Effect");
+            IsShowingHoveringEffect = true;
+        }
+        else
+        {
+            Debug.Log("Set Hovering Effect Off");
+            IsShowingHoveringEffect = false;
+        }
+    }
 }
