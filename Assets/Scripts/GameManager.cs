f5fcebfa1212ed249c4d22c74cfe1a5b2579d589
salvo gurzi <sgurzi@34bigthings.com>
2024-01-27 10:36:52 +0100

    GameManager - added first methods

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,34 +5,22 @@ using UnityEngine;
 
 public enum GameState
 {
-    PLAYER_TURN,
-    ENEMY_TURN,
+    START_MENU,
+    PLAYING,
     GAME_OVER,
-    VICTORY,
-    PLAYING_CARD
+    VICTORY
 }
-public class GameManager : MonoBehaviour
+public class GameManager : Manager<GameManager> 
 {
-    public static GameManager Instance;
-    public CardsContainer cardsDatabase;
+    //public LevelDataAsset dataAsset;
 
     public GameState gameState;
 
-    [SerializeField]
-    private MyPlayer myPlayer;
+    public int levelIndex = 1;
 
-    [SerializeField]
-    private Player enemyPlayer;
-
-    [SerializeField]
-    private int cardsPerTurn = 3;
-
-    public int turnCount = 1;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
+    public Action OnCorrectSequence;
+    public Action OnWrongSequence;
+    public Action<bool> OnGameOver;
 
     void Start()
     {
@@ -41,97 +29,97 @@ public class GameManager : MonoBehaviour
 
     public void initGameValues()
     {
-        myPlayer.initValues();
-        turnCount = 1;
-        gameState = GameState.PLAYER_TURN;
-        enemyPlayer.healthPoints = 10;
-        UIManager.Instance.ClearCardsUI();
-        UIManager.Instance.disableGameEndedScreen();
+
+        gameState = GameState.PLAYING;
+        //UIManager.Instance.ClearCardsUI();
+        //UIManager.Instance.disableGameEndedScreen();
+
+     
     }
 
 
-    public IEnumerator TryPlayCard(CardUI cardObject)
-    {
-        if (cardObject.card.cost <= myPlayer.GetActionPoints() && gameState == GameState.PLAYER_TURN)
-        {
-            gameState = GameState.PLAYING_CARD;
-            myPlayer.PlayCard(cardObject.card, enemyPlayer);
-            UIManager.Instance.Playcard(cardObject);
-            yield return UIManager.Instance.showEventNotification(true, cardObject.card);
+    //public IEnumerator TryPlayCard()
+    //{
+    //    //if (cardObject.card.cost <= myPlayer.GetActionPoints() && gameState == GameState.PLAYER_TURN)
+    //    //{
+    //    //    gameState = GameState.PLAYING_CARD;
+    //    //    myPlayer.PlayCard(cardObject.card, enemyPlayer);
+    //    //    UIManager.Instance.Playcard(cardObject);
+    //    //    yield return UIManager.Instance.showEventNotification(true, cardObject.card);
 
-            Destroy(cardObject.gameObject);
-            gameState = GameState.PLAYER_TURN;
+    //    //    Destroy(cardObject.gameObject);
+    //    //    gameState = GameState.PLAYER_TURN;
 
-            if (enemyPlayer.healthPoints <= 0)
-                callGameEnded(true);
-            else if (!myPlayer.CanStillPlay())
-            {
-                myPlayer.MoveAllToTrash();
-                UIManager.Instance.ClearCardsUI();
-                StartCoroutine(NextTurn());
-            }
+    //    //    if (enemyPlayer.healthPoints <= 0)
+    //    //        callGameEnded(true);
+    //    //    else if (!myPlayer.CanStillPlay())
+    //    //    {
+    //    //        myPlayer.MoveAllToTrash();
+    //    //        UIManager.Instance.ClearCardsUI();
+    //    //        StartCoroutine(NextTurn());
+    //    //    }
 
-        }
-    }
+    //    //}
+    //}
 
-    private IEnumerator NextTurn()
-    {
+    //private IEnumerator NextTurn()
+    //{
 
-        StartCoroutine(UIManager.Instance.changeTurn(gameState == GameState.PLAYER_TURN));
-        yield return new WaitForSeconds(2);
+    //    //StartCoroutine(UIManager.Instance.changeTurn(gameState == GameState.PLAYER_TURN));
+    //    //yield return new WaitForSeconds(2);
 
-        if (gameState == GameState.PLAYER_TURN)
-        {
-            gameState = GameState.ENEMY_TURN;
-            StartCoroutine(PlayEnemyTurn());
-        }
-        else
-        {
-            gameState = GameState.PLAYER_TURN;
-            myPlayer.onTurnStart(cardsPerTurn);
-        }
+    //    //if (gameState == GameState.PLAYER_TURN)
+    //    //{
+    //    //    gameState = GameState.ENEMY_TURN;
+    //    //    StartCoroutine(PlayEnemyTurn());
+    //    //}
+    //    //else
+    //    //{
+    //    //    gameState = GameState.PLAYER_TURN;
+    //    //    myPlayer.onTurnStart(cardsPerTurn);
+    //    //}
 
 
-    }
+    //}
 
-    private void callGameEnded(bool victory)
+    public void GameOver(bool victory)
     {
         if (victory)
+        {
             gameState = GameState.VICTORY;
-        else
-            gameState = GameState.GAME_OVER;
 
-        UIManager.Instance.showGameEndedScreen(victory);
+        }
+        else
+        {
+            gameState = GameState.GAME_OVER;
+        }
+
     }
 
     public void StartNewGame()
     {
+        
         initGameValues();
-        myPlayer.DrawMultipleCards(cardsPerTurn);
 
     }
 
-    private Card DrawRandomcard()
-    {
-        int index = UnityEngine.Random.Range(0, cardsDatabase.cards.Count);
-        return cardsDatabase.cards[index];
-    }
+  
 
-    private IEnumerator PlayEnemyTurn()
-    {
-        Card cardToPlay = DrawRandomcard();
-        enemyPlayer.PlayCard(cardToPlay, myPlayer);
-        yield return UIManager.Instance.showEventNotification(false, cardToPlay);
+    //private IEnumerator PlayEnemyTurn()
+    //{
+    //    Card cardToPlay = DrawRandomcard();
+    //    enemyPlayer.PlayCard(cardToPlay, myPlayer);
+    //    yield return UIManager.Instance.showEventNotification(false, cardToPlay);
 
-        if (myPlayer.healthPoints <= 0)
-            callGameEnded(false);
-        else
-        {
-            turnCount++;
-            StartCoroutine(NextTurn());
-        }
+    //    if (myPlayer.healthPoints <= 0)
+    //        callGameEnded(false);
+    //    else
+    //    {
+    //        turnCount++;
+    //        StartCoroutine(NextTurn());
+    //    }
 
-    }
+    //}
 
     public void QuitGame()
     {
