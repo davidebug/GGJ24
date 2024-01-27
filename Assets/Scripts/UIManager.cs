9b8eede208385c7a0295fd2b8764402073f7d33a
salvo gurzi <sgurzi@34bigthings.com>
2024-01-27 10:37:16 +0100

    UIManager - commented

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,3 @@
-using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -38,78 +37,78 @@ public class UIManager : MonoBehaviour
     //public GameObject gameEndedScreen;
     //private CanvasGroup playerTurnCanvas;
 
-    private void Awake()
-    {
-        //playerTurnCanvas = playerTurn.GetComponent<CanvasGroup>();
-        //Instance = this;
-    }
+    //private void Awake()
+    //{
+    //    playerTurnCanvas = playerTurn.GetComponent<CanvasGroup>();
+    //    Instance = this;
+    //}
 
-    void Update()
-    {
-        //enemyHpCount.text = enemyPlayer.healthPoints.ToString();
-        //enemyHpSlider.value = enemyPlayer.healthPoints;
+    //void Update()
+    //{
+    //    enemyHpCount.text = enemyPlayer.healthPoints.ToString();
+    //    enemyHpSlider.value = enemyPlayer.healthPoints;
 
-        //playerHpCount.text = player.healthPoints.ToString();
-        //playerHpSlider.value = player.healthPoints;
+    //    playerHpCount.text = player.healthPoints.ToString();
+    //    playerHpSlider.value = player.healthPoints;
 
-        //actionPointsCount.text = player.actionPoints.ToString();
-        //deckCount.text = player.deck.Count.ToString();
-        //trashcount.text = player.trash.Count.ToString();
+    //    actionPointsCount.text = player.actionPoints.ToString();
+    //    deckCount.text = player.deck.Count.ToString();
+    //    trashcount.text = player.trash.Count.ToString();
 
-        //currentTurn.text = GameManager.Instance.turnCount.ToString();
+    //    currentTurn.text = GameManager.Instance.turnCount.ToString();
 
-    }
+    //}
 
-    public IEnumerator showEventNotification(bool isMyPlayer, Card card)
-    {
+    //public IEnumerator showEventNotification(bool isMyPlayer, Card card)
+    //{
 
-        //yield return eventNotificator.showEventNotification(isMyPlayer, card);
-    }
+    //    yield return eventNotificator.showEventNotification(isMyPlayer, card);
+    //}
 
-    public IEnumerator changeTurn(bool isEnemyTurn)
-    {
-        //if (isEnemyTurn)
-        //{
-        //    playerTurn.text = "Enemy Turn";
-        //    playerTurn.color = Color.red;
-        //}
-        //else
-        //{
-        //    playerTurn.text = "Your Turn";
-        //    playerTurn.color = Color.green;
-        //}
-        //playerTurnCanvas.alpha = 0;
-        //Vector3 startPosition = playerTurn.transform.position;
-        //playerTurn.transform.position = new Vector3(Screen.width / 2, Screen.height / 1.5F, 0);
-        //playerTurn.transform.localScale *= 3;
-        //playerTurnCanvas.DOFade(1, 0.5F);
-        //playerTurn.transform.DOScale(1F, 0.5F);
-        //yield return new WaitForSeconds(1);
-        //playerTurn.transform.DOMove(startPosition, 0.5F);
- 
-        
-  
-        
-    }
+    //public IEnumerator changeTurn(bool isEnemyTurn)
+    //{
+    //    if (isEnemyTurn)
+    //    {
+    //        playerTurn.text = "Enemy Turn";
+    //        playerTurn.color = Color.red;
+    //    }
+    //    else
+    //    {
+    //        playerTurn.text = "Your Turn";
+    //        playerTurn.color = Color.green;
+    //    }
+    //    playerTurnCanvas.alpha = 0;
+    //    Vector3 startPosition = playerTurn.transform.position;
+    //    playerTurn.transform.position = new Vector3(Screen.width / 2, Screen.height / 1.5F, 0);
+    //    playerTurn.transform.localScale *= 3;
+    //    playerTurnCanvas.DOFade(1, 0.5F);
+    //    playerTurn.transform.DOScale(1F, 0.5F);
+    //    yield return new WaitForSeconds(1);
+    //    playerTurn.transform.DOMove(startPosition, 0.5F);
 
-    public void Playcard(CardUI cardObject)
-    {
-      //  cardObject.PlayCardAnimation();
- 
-    }
 
-    public void IstanceCardObject(Card cardToIstance, int position)
-    {
-        //GameObject cardPrefab = Instantiate((GameObject)Resources.Load("Prefabs/CardPrefab"),
-        //    cardsPositions[position].transform.position,
-        //    Quaternion.identity,
-        //    cardsPositions[position].transform);
 
-        //CardUI cardUI = cardPrefab.GetComponent<CardUI>();
-        //cardUI.card = cardToIstance;
-        //cardsObjects.Add(cardPrefab);
 
-    }
+    //}
+
+    //public void Playcard(CardUI cardObject)
+    //{
+    //    cardObject.PlayCardAnimation();
+
+    //}
+
+    //public void IstanceCardObject(Card cardToIstance, int position)
+    //{
+    //    GameObject cardPrefab = Instantiate((GameObject)Resources.Load("Prefabs/CardPrefab"),
+    //        cardsPositions[position].transform.position,
+    //        Quaternion.identity,
+    //        cardsPositions[position].transform);
+
+    //    CardUI cardUI = cardPrefab.GetComponent<CardUI>();
+    //    cardUI.card = cardToIstance;
+    //    cardsObjects.Add(cardPrefab);
+
+    //}
 
     //public void showGameEndedScreen(bool victory)
     //{
