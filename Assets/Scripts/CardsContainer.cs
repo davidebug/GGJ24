c4ff436cf7b1c64ebd57f9205b6721a0df27090d
Davide Bagnato <davidebagnato97@gmail.com>
2024-01-27 10:05:11 +0100

    Removed stuff

--- Assets/Scripts/CardsContainer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Cards Container", menuName = "Cards/CardsContainer")]
-public class CardsContainer : ScriptableObject
-{
-    public List<Card> cards;
-}
