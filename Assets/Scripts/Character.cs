241723aff21692d8e55c85f35b0682d8fd453ea8
salvo gurzi <sgurzi@34bigthings.com>
2024-01-27 11:36:06 +0100

    Character -  added script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public BodyPart[] bodyParts;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+}
