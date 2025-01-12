f10d52cb803b709eecb59e16767a063fe67d7142
salvolannister <darksawyer@hotmail.it>
2025-01-13 00:57:18 +0100

    audio - fade between two gameplay songs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,7 +9,9 @@ public class AudioManager : Manager<AudioManager>
     public AudioAssetScriptableObject AudioAssetSO;
     public AudioSource audioSource;
     public AudioSource audioSource2;
-
+    public AudioSource exitingSource;
+    private const float GAMEPLAY_TARGET_VOLUME = 0.3f;
+    private AudioClip exitingSong;
     // Start is called before the first frame update
     int nChanges = 0;
     public void Awake()
@@ -24,42 +26,59 @@ public class AudioManager : Manager<AudioManager>
     void Start()
     {
         Assert.IsNotNull(AudioAssetSO);
-        //GameManager.Get().OnGameStateChanged += SwitchSongBasedOnGameState;
+        GameManager.Get().OnGameStateChanged += SwitchSongBasedOnGameState;
         audioSource.loop = true;
         audioSource.clip = AudioAssetSO.gamePlayMusic_2;
         audioSource.Play();
     }
 
-    //private void SwitchSongBasedOnGameState(GameState state)
-    //{
-    //    switch (state)
-    //    {
-    //        case GameState.SOLUTION:
-    //            if (AudioAssetSO.mainMenuSongTheme)
-    //            {
-    //                //int nAudioClip = nChanges % 2;
-    //                // original idea was to fade from one song to another
-    //                audioSource.Stop();
-                   
-    //                //nChanges++;
-    //            }
-    //            break;
-    //        default: break;
+    private void SwitchSongBasedOnGameState(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.SOLUTION:
 
-    //    }
-    //}
+                StartCoroutine(PlayGameplayAudioSwitchCoroutine());
 
-    //IEnumerator  PlayGameplayAudioSwitchCoroutine()
-    //{
+                break;
 
-    //    yield return;
-    //}
+            default: break;
+
+        }
+    }
+
+
+
+    IEnumerator PlayGameplayAudioSwitchCoroutine()
+    {
+        //Improve and create a general fade In and out
+        float exitingSomeExecutionTime = audioSource.time;
+        AudioClip exitingSong = audioSource.clip;
+        AudioClip playingSong = exitingSong == AudioAssetSO.gamePlayMusic_1 ? AudioAssetSO.gamePlayMusic_2 : AudioAssetSO.gamePlayMusic_1;
+        float startVolume = audioSource.volume;
+        float elapsedTime = 0f;
+        audioSource.volume = 0f;
+        exitingSource.clip = exitingSong;
+        exitingSource.time = exitingSomeExecutionTime;
+        exitingSource.volume = startVolume;
+        audioSource.clip = playingSong;
+        audioSource.time = 0.0f;
+        audioSource.Play();
+        exitingSource.Play();
+        while (elapsedTime < 5.0f)
+        {
+            elapsedTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(0, GAMEPLAY_TARGET_VOLUME, elapsedTime / 5.0f);
+            exitingSource.volume = Mathf.Lerp(GAMEPLAY_TARGET_VOLUME, 0, elapsedTime / 5.0f);
+            yield return null;
+        }
+    }
 
     public void PlayingWithSecondAudioSource(AudioClip clip)
     {
         if (audioSource2.isPlaying)
         {
-            audioSource2.Stop();    
+            audioSource2.Stop();
         }
 
         audioSource.PlayOneShot(clip);
@@ -67,16 +86,16 @@ public class AudioManager : Manager<AudioManager>
 
     public void PlayLaugh(int levelIndex)
     {
-        PlayingWithSecondAudioSource(AudioAssetSO.GetRandomAudioLaugh(levelIndex));   
+        PlayingWithSecondAudioSource(AudioAssetSO.GetRandomAudioLaugh(levelIndex));
     }
 
     public void PlayWoorp()
     {
-        PlayingWithSecondAudioSource(AudioAssetSO.GetRandomWoorp());    
+        PlayingWithSecondAudioSource(AudioAssetSO.GetRandomWoorp());
     }
 
     public void PlayWhoosh()
     {
-        PlayingWithSecondAudioSource(AudioAssetSO.GetRandomWhoosh());   
+        PlayingWithSecondAudioSource(AudioAssetSO.GetRandomWhoosh());
     }
 }
